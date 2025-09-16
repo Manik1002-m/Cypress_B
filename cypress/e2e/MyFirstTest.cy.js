fbdd38360568d68e60db11cfe0fafba851899fb0
Manik1002-m <manik@sqelabs.com>
2025-09-16 16:59:15 +0530

    FirstTest and Locators

--- cypress/e2e/MyFirstTest.cy.js
+++ cypress/e2e/MyFirstTest.cy.js
@@ -3,6 +3,8 @@ describe('My First Test', () => {
   it('verify title-positive', () => {
     cy.visit("https://opensource-demo.orangehrmlive.com/")
     cy.title().should('eq', 'OrangeHRM') 
+    cy.pause()
+    cy.title().should('eq', 'OrangeHRM')
   })
 
   it('verify title-negative', () =>{
